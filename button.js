f011b32be117a0802f74e9210cb68e9253a41786
armelledev <laeticiaarmelle46@gmail.com>
2025-07-15 14:30:27 +0200

    introduction en javasript

---
+++ button.js
@@ -0,0 +1,16 @@
+let btn =document.querySelector("button");
+console.log(btn);
+
+btn.addEventListener('click',
+   () =>{
+    alert('click du boutton')
+   }
+    
+    );
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,60 @@
+let nbre=3;
+
+nbre=14
+console.log(nbre);
+const nbre2=5;
+
+
+
+let firstName ="waffo";
+console.log(firstName);
+
+let lastName ="rostand";
+console.log(lastName);
+
+let fullName ="waffo rostand"
+console.log(fullName);
+
+console.log(typeof(lastName));
+console.log(typeof(nbre));
+
+let fruits=['orange','bluebery','banana','watermelon'];
+
+let fruits2=['strawberry', 'pineaple', 'apple', 'kiwi'];
+
+
+console.log(fruits2);
+console.log('');
+// console.log(typeof(fruits2))
+
+// document.write(fruits2);
+
+// alert('hello world');
+// prompt('entrer votre nom');
+
+// let nom="";
+// nom =prompt('entre votre nom');
+// document.write("bonjour prof " +nom);
+// som = parseInt(a) + parseInt(b);
+
+
+
+// let a=prompt('entre nombre 01');
+// let b=prompt('entre nombre 02');
+
+// let som=0;
+// let sous=0;
+// a=parseInt(a);
+// b=parseInt(b);
+
+// som = a + b;
+// sous = a - b;
+
+// if(a>b){
+// document.write("la soustraction est :" +sous);
+// }
+// else if(a<b){
+//     document.write("la somme est :" +som);
+// }
+
+// var nbre3=17;
